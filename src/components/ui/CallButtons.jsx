0541fd85f38c281b0572ccbaff64553b53b6ceac
mdleion9 <mdleionmdleion50@gmail.com>
2026-04-17 22:04:14 +0600

    Call button and Friend card section

--- src/components/ui/CallButtons.jsx
+++ src/components/ui/CallButtons.jsx
@@ -1,1 +1,59 @@
- 
+"use client";
+import { frContext } from "@/context/FriendContext";
+import React, { useContext } from "react";
+import { FiPhoneCall } from "react-icons/fi";
+import { PiChatText, PiVideoCameraBold } from "react-icons/pi";
+import { toast } from "react-toastify";
+
+const CallButtons = ({ friend }) => {
+  const { friendData, setFriendData } = useContext(frContext);
+  //   console.log(friendData, setFriendData);
+  // console.log(friend)
+
+  const handleCall = (type) => {
+    const newEntry = {
+      id: Date.now(),
+      friend,
+      type,
+      timestamp: new Date().getTime(),
+    };
+    setFriendData([...friendData, newEntry]);
+
+    toast.success(`${newEntry.type} ${newEntry.friend.name}`,{
+      position:"top-center",
+      autoClose:1500
+      
+    });
+  };
+  //   console.log(friendData);
+
+  return (
+    <div>
+      <div className="grid grid-cols-3  gap-4">
+        <div
+          onClick={() => handleCall("audio")}
+          className="flex flex-col items-center border border-zinc-200 rounded-md p-2 md:p-5 cursor-pointer transition-all duration-500 hover:scale-105 select-none"
+        >
+          <FiPhoneCall className="text-xl font-bold" />
+          <p>Call</p>
+        </div>
+        <div
+          onClick={() => handleCall("text")}
+          className="flex flex-col items-center border border-zinc-200 rounded-md p-2 md:p-5 cursor-pointer transition-all duration-500 hover:scale-105 select-none"
+        >
+          <PiChatText className="text-xl font-bold" />
+          <p>Text</p>
+        </div>
+        <div
+          onClick={() => handleCall("video")}
+          className="flex flex-col items-center border border-zinc-200 rounded-md p-2 md:p-5 cursor-pointer transition-all duration-500 hover:scale-105 select-none"
+        >
+          <PiVideoCameraBold className="text-xl font-bold" />
+          <p>Video</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CallButtons;
--- src/components/ui/FriendCart.jsx
+++ src/components/ui/FriendCart.jsx
@@ -1,1 +1,61 @@
- 
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const FriendCart = ({ friend }) => {
+  
+  const { id, name, status, picture, days_since_contact, tags } = friend;
+
+  const statusBg = {
+    "almost due": "bg-[#EFAD44]",
+    "on-track": "bg-[#244D3F]",
+    overdue: "bg-[#EF4444]",
+  };
+
+  return (
+    <Link href={`/friends/${id}`}>
+      <div className="relative group p-[3px] overflow-hidden rounded-2xl bg-zinc-100 h-full mx-4 lg:mx-0">
+        <div className="absolute inset-[-1000%] bg-[conic-gradient(from_0deg,transparent_25%,#22c55e,transparent_75%)] animate-border-rotate md:opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
+
+        <div className="relative border-2 border-zinc-100 rounded-xl p-6 flex flex-col items-center justify-center space-y-3 h-full bg-white z-8">
+          <Image
+            className="rounded-full  shadow-green-500 
+          border-2 border-transparent
+          backdrop-blur-2xl 
+          bg-white/10
+          transition-all
+          duration-500 
+          hover:border-green-500
+          hover:shadow-green-500/40
+          hover:scale-95 
+          hover:border-2 
+          hover:shadow-xl"
+            width={100}
+            height={100}
+            src={picture}
+            alt={name}
+          />
+          <h3 className="text-xl md:text-2xl font-bold">{name}</h3>
+          <p>{days_since_contact}d ago</p>
+          <div className="flex gap-2">
+            {tags.map((tag, i) => (
+              <p
+                key={i}
+                className="bg-[#CBFADB] px-2 py-1 text-[12px] rounded-full font-bold"
+              >
+                {tag}
+              </p>
+            ))}
+          </div>
+          <p
+            className={`${statusBg[status]} px-2 py-1 rounded-full text-white text-[12px] font-bold `}
+          >
+            {status}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default FriendCart;
