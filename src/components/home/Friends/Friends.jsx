39ab287469194aaf100bbccd1fedde311dc289d9
mdleion9 <mdleionmdleion50@gmail.com>
2026-04-17 21:55:09 +0600

    Friends.jsx and home page compeled

--- src/components/home/Friends/Friends.jsx
+++ src/components/home/Friends/Friends.jsx
@@ -1,1 +1,27 @@
- 
+import FriendCart from '@/components/ui/FriendCart';
+import React from 'react';
+
+export const dynamic = 'force-dynamic';
+
+const PromiseData = async () => {
+  const res = await fetch('http://localhost:3000/friendData.json', {
+  cache: 'no-store'
+});
+
+  const data = await res.json();
+  return data.friends;
+};
+
+const Friends = async () => {
+  const friends = await PromiseData();
+
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+      {friends.map(friend => (
+        <FriendCart key={friend.id} friend={friend} />
+      ))}
+    </div>
+  );
+};
+
+export default Friends;
