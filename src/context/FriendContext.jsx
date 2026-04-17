a04da2d27f754e6dac5cac7e0f01fdfa91278ed6
mdleion9 <mdleionmdleion50@gmail.com>
2026-04-17 22:05:20 +0600

    Friend context

--- src/context/FriendContext.jsx
+++ src/context/FriendContext.jsx
@@ -1,1 +1,23 @@
- 
+"use client"
+
+import React, { createContext, useState } from 'react';
+export const frContext = createContext()
+
+const FriendContext = ({children}) => {
+
+    const [friendData, setFriendData]=useState([])
+    const data ={
+       friendData, 
+       setFriendData
+    }
+
+    return (
+        <div>
+            <frContext.Provider value={data}>
+                {children}
+            </frContext.Provider>
+        </div>
+    );
+};
+
+export default FriendContext;
